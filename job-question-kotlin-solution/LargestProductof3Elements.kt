2a66a89a9ba4198cf53c174d9247ed5d885b1af4
recca <flamerecca711@gmail.com>
2019-09-02 09:22:17 +0800

    Add LargestProductof3Elements.kt

---
+++ job-question-kotlin-solution/LargestProductof3Elements.kt
@@ -0,0 +1,8 @@
+fun main(arguments: Array<String>) {
+    val numbers = intArrayOf(0, 0, 0, 2, 0, 1, 3, 4, 0, 0)
+    println(maximum_product_of_three(numbers))
+}
+
+fun intArray maximum_product_of_three(numbers: intArray) {
+    return numbers
+}
